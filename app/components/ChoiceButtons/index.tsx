9dcc157b9f6e791e1a29f330856eb6cf28f5cfad
Алексей А. Хлебаев <akhlebaev@yoomoney.ru>
2024-11-09 18:32:37 +0300

    redisign main page

---
+++ app/components/ChoiceButtons/index.tsx
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+import { Row, Col, Typography, List, Button } from 'antd';
+
+const { Title, Paragraph } = Typography;
+
+import styles from "./calendar.module.css";
+
+// Стили в формате inline для React
+const listStyle = {
+  borderRadius: '8px', // Скругленные углы
+  margin: '0 auto', // Выравнивание по центру
+};
+
+const itemStyle = {
+  fontSize: '18px', // Размер шрифта
+  paddingBottom: '10px', // Отступ снизу для каждого пункта
+  lineHeight: '1.6', // Межстрочный интервал
+  textAlign: 'left',
+  color: 'white', // Белый текст
+};
+const markerStyle = {
+};
+
+const GradientList = ({data}: {data:  string[]}) => {
+  return (
+    <div style={listStyle}>
+      <ul className={styles.gl} style={markerStyle}>
+        {data.map((item, index) => (
+          <li key={index} 
+          
+          style={itemStyle}>
+            {item}
+          </li> 
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const ChoiceButtons = () => {
+
+
+  return (
+    <Row style={{ minHeight: '57vh', marginBottom: '-25px' }}>
+      {/* Левая колонка */}
+      <Col
+        span={12}
+        className="column"
+        style={{
+          backgroundColor: 'rgba(0,0,0, 0.5)',
+          display: 'flex',
+          alignItems: 'top',
+          justifyContent: 'center',
+          padding: '20px 40px',
+          transition: 'all 1s',
+          cursor: 'pointer'
+        }}
+      >
+        <div style={{ textAlign: 'center', width: '100%', display: 'flex', 'alignItems': 'top',     justifyContent: 'center' }}>
+          <div 
+        style={{
+          width: '350',
+          color: 'white',
+          paddingTop: '50px'
+        }}
+        >
+          <Title className="text-white" style={{ textAlign: 'center', color: '#fff'}} level={2}>For Candidate</Title>
+          <Paragraph className="text-gray-300" style={{ fontSize: '18px', marginTop: '-6px'}}>Explore career and wellness options</Paragraph>
+
+            <>
+              <GradientList
+                data={[
+                  'Choose your hard skills',
+                  'Understand your wellbeing priorities',
+                  'Get job list',
+                ]}
+              />
+            </>
+              <Button type="primary" href='/candidate/chat' size='large' style={{width: '100%'}}>Create Your Profile</Button>
+        </div>
+        </div>
+      </Col>
+      <Col
+        span={12}
+        className="column"
+        style={{
+          backgroundColor: 'rgba(0,0,0, 0.5)',
+          display: 'flex',
+          alignItems: 'top',
+          justifyContent: 'center',
+          padding: '20px 40px',
+          transition: 'opacity 0.5s ease',
+          cursor: 'pointer'
+        }}
+      >
+     <div style={{ textAlign: 'center', width: '100%', display: 'flex', 'alignItems': 'top',     justifyContent: 'center' }}>
+        <div 
+      style={{
+        width: '350px',
+          paddingTop: '50px'
+      }}
+      >
+         
+        <Title  style={{ textAlign: 'center', color: '#fff'}} level={2}>For Companies</Title>
+        <Paragraph className="text-gray-300"  style={{fontSize: '18px', marginTop: '-6px'}}>Empower your company wellbeing culture</Paragraph>
+            <>
+              <GradientList
+                data={[
+                  'Grow your wellbeing culture',
+                  'Analyze your company',
+                  'Get candidate list',
+                ]}
+              />
+              
+            </>
+           
+   
+          <Button size='large' href='/candidate/chat'  type="primary" style={{width: '100%'}}>Try Demo</Button>
+        </div>
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+export default ChoiceButtons;
--- app/page.tsx
+++ app/page.tsx
@@ -3,52 +3,67 @@ import React from 'react';
 import { Layout, Menu, Button, Row, Col, Card, Typography, Upload } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import Link from 'antd/es/typography/Link';
+import ChoiceButtons from './components/ChoiceButtons';
 
 const { Header, Content, Footer } = Layout;
 const { Title, Text } = Typography;
 
 const AppLandingPage = () => {
   return (
-    <Layout>
+    <Layout style={{minHeight: '100vh' }}>
       {/* Header */}
       <Header style={{ position: 'fixed', width: '100%', zIndex: 1 }}>
         <div className="logo" style={{ float: 'left', color: '#fff', fontSize: 24, paddingRight: '48px' }}>Tender</div>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+       {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
           <Menu.Item key="1"><Link href="#home">Home</Link></Menu.Item>
           <Menu.Item key="2"><Link href="#features">Features</Link></Menu.Item>
           <Menu.Item key="3"><Link href="#get-started">Get Started</Link></Menu.Item>
-        </Menu>
+        </Menu> */}
       </Header>
+
+      
 
       {/* Main Content */}
       <a id="home" />
-      <Content style={{ padding: '50px 50px', marginTop: 64 }}>
+      <Content style={{ padding: '0', marginTop: 64, flex: 1}}
+      >
         {/* Hero Section */}
         <Row justify="center" align="middle" style={{
-          minHeight: '60vh',
-          backgroundImage: 'url("/hero.jpg")',
+          minHeight: 'calc(100vh - 150px)',
+          backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("/hero.jpg")',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backdropFilter: 'blur(5px)',
           color: '#fff',
           textAlign: 'center',
-          padding: '60px 20px'
         }}>
-          <Col span={12}>
-            <Title level={1} style={{color: 'white'}}>Find Your Perfect Fit</Title>
-            <Text style={{color: 'white'}}>
-              Connect with companies that share your interests and values. Let us help you discover career opportunities that align with both your professional and personal passions.
-            </Text>
-            <br />
-            <Link href="/candidate/chat">
-              <Button type="primary" size="large" style={{ marginTop: 16 }}>
-                Get Started
-              </Button>
-            </Link>
-          </Col>
+        <Col span={32}>
+        <div className="flex space-x-3 justify-center" 
+        style={{
+          borderRadius: '20px',
+          padding: '0 20px'
+        }}>
+            <div className="text-center   max-w-3xl">
+                <h1 className="text-white text-4xl sm:text-5xl font-bold mb-4" style={{
+                      backgroundImage: 'linear-gradient(to right, rgb(255, 30, 140) 10%, rgb(255, 102, 57), rgb(255, 230, 29) 40%, rgb(255, 230, 29), rgb(255, 230, 29) 60%, rgb(87, 229, 98), rgb(31, 179, 253) 85%, rgb(31, 179, 253), rgb(31, 179, 253))', /* Your gradient colors */
+
+          WebkitBackgroundClip: 'text',
+                      color: 'transparent' /* Make the text itself transparent */
+                }}>
+                Find the Right Fit for You: <br/> Where Values and Careers Align
+                </h1>
+                <p className="text-gray-300 text-lg sm:text-xl mb-8">
+                Join us to open a job where your values, lifestyle and mental health come first.
+                </p>
+            </div>
+       </div>
+       </Col>
+        <Col span={24}>
+              <ChoiceButtons />
+        </Col>
         </Row>
 
-        {/* Features Section */}
+        {/* Features Section 
         <a id="features" />
         <Row gutter={16} style={{ marginTop: 32 }}>
           <Col span={8}>
@@ -66,9 +81,13 @@ const AppLandingPage = () => {
               Schedule learning sessions directly into your calendar, ensuring you stay on track with your career growth.
             </Card>
           </Col>
-        </Row>
+        </Row>*/}
 
-        {/* Profile Setup Section */}
+
+
+
+
+        {/* Profile Setup Section 
         <a id="get-started" />
         <Row justify="center" style={{ marginTop: 64 }}>
           <Col span={12} style={{ textAlign: 'center' }}>
@@ -85,7 +104,7 @@ const AppLandingPage = () => {
               <Button icon={<UploadOutlined />} style={{ marginTop: 8 }}>Upload Resume (PDF)</Button>
             </Link>
           </Col>
-        </Row>
+        </Row>*/}
       </Content>
 
       {/* Footer */}
