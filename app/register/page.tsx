acbe4df8ac7181ad3610298e3b9b200c6fb55c0d
Aleksandr Shoronov <alexshoronov@gmail.com>
2024-11-10 10:53:26 +0200

    Added demo notice

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -27,7 +27,10 @@ export default function Login() {
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
           <h3 className="text-xl font-semibold">Sign Up</h3>
           <p className="text-sm text-gray-500">
-            Create an account with your email and password
+            Create an account with your email and password.
+          </p>
+          <p style={{color: 'red'}}>
+            Hey, Junction participant! You can use hacker@gmail.com and password 1234 to view demo content if you want, or register user otherwise.
           </p>
         </div>
         <Form action={register}>
